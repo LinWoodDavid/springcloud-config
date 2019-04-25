d95360c751d7eed6d0da49c0fe9910d696cd78cf
LinWoodDavid <17610897521@163.com>
2019-04-25 18:12:24 +0800

    spring-cloud-config github配置文件手动刷新

--- config-client/src/main/java/com/david/web/HelloController.java
+++ config-client/src/main/java/com/david/web/HelloController.java
@@ -16,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope   //动态刷新
 public class HelloController {
 
+    /**
+     * 更改github上面的文件 将version=6改为7并保存
+     * 刷新配置:
+     * 使用post请求访问 http://localhost:6090/actuator/refresh 请求头Content-Type为application/json
+     * 再次访问http://localhost:6090/hello 显示foo=foo ,version=7
+     *
+     */
     @Value("${foo}")
     String foo;
 
