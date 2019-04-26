7e08bdaa25757e766f85261d8f1f4888bbb20beb
LinWoodDavid <17610897521@163.com>
2019-04-26 18:44:49 +0800

    2019.04.26

--- config-server/src/main/java/com/david/ConfigServerApplication.java
+++ config-server/src/main/java/com/david/ConfigServerApplication.java
@@ -2,9 +2,13 @@ package com.david;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableConfigServer//开启配置服务器的功能
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
