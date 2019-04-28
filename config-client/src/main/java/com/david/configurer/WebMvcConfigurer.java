6dfbd71bac80fa39adced89baa125b7a0d30eba0
LinWoodDavid <17610897521@163.com>
2019-04-28 09:47:16 +0800

    2019.04.28 spring cloud config 自动更新配置

---
+++ config-client/src/main/java/com/david/configurer/WebMvcConfigurer.java
@@ -0,0 +1,40 @@
+package com.david.configurer;
+
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Spring MVC 配置
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurationSupport {
+
+    private final Logger logger = LoggerFactory.getLogger(WebMvcConfigurer.class);
+
+    //使用阿里 FastJson 作为JSON MessageConverter
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        //config.setSerializerFeatures(SerializerFeature.WriteMapNullValue);//保留空的字段
+        //SerializerFeature.WriteNullStringAsEmpty,//String null -> ""
+        //SerializerFeature.WriteNullNumberAsZero//Number null -> 0
+        // 按需配置，更多参考FastJson文档哈
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");//日期格式
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(Charset.forName("UTF-8"));
+        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON_UTF8));
+        converters.add(converter);
+    }
+
+}
